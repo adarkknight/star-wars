ac2ae53488e297139089a968754258c7d2db1fc7
Christopher Brock <brock.christopherj@gmail.com>
2024-01-03 12:49:33 -0600

    Wiring up button

--- src/components/DataLinkCard.tsx
+++ src/components/DataLinkCard.tsx
@@ -3,13 +3,18 @@ interface Props {
 }
 
 const DataLinkCard = ({ title }: Props) => {
+  const handleClick = () => {
+    return console.log("You clicked me!");
+  };
   return (
     <div className="w-full md:w-64 mt-3 mb-3">
       <div className="card bg-black shadow-xl">
         <div className="card-body items-center text-center">
           <h2 className="card-title text-white">{title}</h2>
           <div className="card-actions">
-            <button className="btn btn-primary">Data</button>
+            <button className="btn btn-primary" onClick={handleClick}>
+              Data
+            </button>
           </div>
         </div>
       </div>
